23e852db0f3aa0a05785a28d585dde649f776551
andre-aion <andre@aion.network>
2019-01-10 00:30:40 -0400

    working perfectly with flask using iframe

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ from bokeh.document import without_document_lock
 # Bokeh basics
 from bokeh.models.widgets import Tabs
 from bokeh.server.server import Server
+from bokeh.application import Application
+from bokeh.application.handlers.function import FunctionHandler
+
 
 # GET THE DASHBOARDS
+from tornado.ioloop import IOLoop
+
 from scripts.dashboards.blockminer import blockminer_tab
 from scripts.dashboards.churn import churn_tab
 from scripts.dashboards.hashrate import hashrate_tab
@@ -29,10 +34,26 @@ def aion_analytics(doc):
         #pm = yield poolminer_tab()
 
         tabs = Tabs(tabs=[bm])
+        #doc = curdoc()
         doc.add_root(tabs)
 
     except Exception:
         logger.error("TABS:", exc_info=True)
 
-doc = curdoc()
-aion_analytics(doc)
+
+# configure and run bokeh server
+@gen.coroutine
+@without_document_lock
+def launch_server():
+    try:
+        apps = {"/aion-analytics": Application(FunctionHandler(aion_analytics))}
+        io_loop = IOLoop.current()
+        server = Server(apps,port=5006,allow_websocket_origin=["*"],io_loop=io_loop)
+        server.start()
+        server.io_loop.add_callback(server.show, '/aion-analytics')
+        server.io_loop.start()
+    except Exception:
+        logger.error("WEBSERVER LAUNCH:", exc_info=True)
+
+if __name__ == '__main__':
+    launch_server()
