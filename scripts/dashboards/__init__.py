fdb995c0422719890be4825a6bd4a45e278a088b
andre-aion <andre@aion.network>
2019-01-23 00:28:27 -0400

    after merge, after rollback, working

--- scripts/dashboards/__init__.py
+++ scripts/dashboards/__init__.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
 from . import poolminer, blockminer
-=======
-from . import hashrate, poolminer, blockminer
->>>>>>> 33a833d6950c4a454d08eb9945159ff9da9f68a7
+from . import  poolminer, blockminer
 from scripts.dashboards.churn import tier1_miner_predictive, churn
